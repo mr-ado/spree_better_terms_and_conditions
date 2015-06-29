70c963243002426d74ec2908f735a997a5f37ebc
Ado <adrianbeyerle@gmail.com>
2015-06-29 14:42:28 +1000

    Remove payment validation

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,5 @@
 Spree::Order.class_eval do
 
-  # attr_accessible :terms_and_conditions # uncomment for Spree below version 2.1
 
   # Add new checkout step to checkout process
   insert_checkout_step :terms_and_conditions, :before => :confirm
@@ -12,21 +11,10 @@ Spree::Order.class_eval do
       self.errors[:terms_and_conditions].empty? ? true : false
     end
   end
-
-  def payment_selected?
-    unless payments.valid.size >= 1
-      # errors.add(:base, 'Terms and Conditions must be accepted!')
-      self.errors[:payments] << 'You have to select a payment.'
-      self.errors[:payments].empty? ? true : false
-    end
-  end
 end
 
 # Validate on state change
 Spree::Order.state_machine.before_transition :to => :confirm, :do => :valid_terms_and_conditions?
 
-# Validate on state change
-Spree::Order.state_machine.before_transition :to => :terms_and_conditions, :do => :payment_selected?
-
 # Add terms_and_conditions to strong parameters
-Spree::PermittedAttributes.checkout_attributes << :terms_and_conditions # Remove if Spree is below version 2.1
+Spree::PermittedAttributes.checkout_attributes << :terms_and_conditions
